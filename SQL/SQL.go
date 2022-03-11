860914a239e669a17ae2bc473c8b04b0c07c3fd7
lizhang <1710555103@qq.com>
2022-03-11 09:39:52 +0800

    v0.93

--- SQL/SQL.go
+++ SQL/SQL.go
@@ -3,7 +3,6 @@ package SQL
 import (
 	"database/sql"
 	_ "database/sql/driver"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/bcrypt"
 	"log"
@@ -30,39 +29,39 @@ func InitMysql() *sql.DB {
 	return DB
 }
 
-// QueryRow 查询单条数据示例
-func QueryRow(db *sql.DB, userid int) {
-	sqlStr := "select id, name, age from bubble where id=?"
-	var u User
-	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
-	err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
-	//rows,err := db.Query(sqlStr, "xys")//多行查询用Query
-	if err != nil {
-		fmt.Printf("scan failed, err:%v\n", err)
-		return
-	}
-	fmt.Printf("ID:%d Title:%s Status:%d\n", u.ID, u.Title, u.Status)
-}
-func Query(db *sql.DB) {
-	sqlStr := "select * from bubble;"
-	var u User
-	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
-	//err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
-	rows, err := db.Query(sqlStr, "xys") //多行查询用Query
-	if err != nil {
-		fmt.Printf("scan failed, err:%v\n", err)
-		return
-	}
-	for rows.Next() {
-		rows.Scan(&u.ID, &u.Title, &u.Status)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("get data, ID: %d, Title: %s, Status: %d", u.ID, u.Title, u.Status)
-	}
-
-	fmt.Printf("ID:%d Title:%s Status:%d\n", u.ID, u.Title, u.Status)
-}
+// Query // QueryRow 查询单条数据示例
+//func QueryRow(db *sql.DB, userid int) {
+//	sqlStr := "select id, name, age from bubble where id=?"
+//	var u User
+//	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+//	err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
+//	//rows,err := db.Query(sqlStr, "xys")//多行查询用Query
+//	if err != nil {
+//		fmt.Printf("scan failed, err:%v\n", err)
+//		return
+//	}
+//	fmt.Printf("ID:%d Title:%s Status:%d\n", u.ID, u.Title, u.Status)
+//}
+//func Query(db *sql.DB) {
+//	sqlStr := "select * from bubble;"
+//	var u User
+//	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+//	//err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
+//	rows, err := db.Query(sqlStr, "xys") //多行查询用Query
+//	if err != nil {
+//		fmt.Printf("scan failed, err:%v\n", err)
+//		return
+//	}
+//	for rows.Next() {
+//		err=rows.Scan(&u.ID, &u.Title, &u.Status)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		log.Printf("get data, ID: %d, Title: %s, Status: %d", u.ID, u.Title, u.Status)
+//	}
+//
+//	fmt.Printf("ID:%d Title:%s Status:%d\n", u.ID, u.Title, u.Status)
+//}
 
 func CreatAccount(db *sql.DB, user *User) uint8 {
 	//对
@@ -85,38 +84,39 @@ func CreatAccount(db *sql.DB, user *User) uint8 {
 	return 200
 }
 
-func DeleteDB(db *sql.DB, ID string) {
-	_, err := db.Exec(`delete from bubble where id=?;`, ID)
-	//defer rows.Close()
-	if err != nil {
-		log.Fatalf("Delete data error: %v\n", err)
-	}
-	//var result int
-	//rows.Scan(&result)
-	log.Printf("Delete Ok!")
-}
+//func DeleteDB(db *sql.DB, ID string) {
+//	_, err := db.Exec(`delete from bubble where id=?;`, ID)
+//	//defer rows.Close()
+//	if err != nil {
+//		log.Fatalf("Delete data error: %v\n", err)
+//	}
+//	//var result int
+//	//rows.Scan(&result)
+//	log.Printf("Delete Ok!")
+//}
+//
+//func FindDB(db *sql.DB, todoList []User) []User {
+//	sqlStr := "select * from bubble "
+//	var u User
+//	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
+//	//err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
+//	rows, err := db.Query(sqlStr) //多行查询用Query
+//	if err != nil {
+//		fmt.Printf("scan failed, err:%v\n", err)
+//		return make([]User, 0)
+//	}
+//	for rows.Next() {
+//		err := rows.Scan(&u.ID, &u.Title, &u.Status)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//		todoList = append(todoList, u)
+//
+//		log.Printf("get data, ID: %d, Title: %s, Status: %d", u.ID, u.Title, u.Status)
+//	}
+//	return todoList
+//}
 
-func FindDB(db *sql.DB, todoList []User) []User {
-	sqlStr := "select * from bubble "
-	var u User
-	// 非常重要：确保QueryRow之后调用Scan方法，否则持有的数据库链接不会被释放
-	//err := db.QueryRow(sqlStr, userid).Scan(&u.ID, &u.Title, &u.Status) //单行查询用QueryRow
-	rows, err := db.Query(sqlStr) //多行查询用Query
-	if err != nil {
-		fmt.Printf("scan failed, err:%v\n", err)
-		return make([]User, 0)
-	}
-	for rows.Next() {
-		err := rows.Scan(&u.ID, &u.Title, &u.Status)
-		if err != nil {
-			log.Fatal(err)
-		}
-		todoList = append(todoList, u)
-
-		log.Printf("get data, ID: %d, Title: %s, Status: %d", u.ID, u.Title, u.Status)
-	}
-	return todoList
-}
 func TodoIsOK(user *User) (bool, string) {
 	//匹配电子邮箱
 	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*` //匹配电子邮箱
